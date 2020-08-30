5b4cda2459b95dd537d02cbc5decf7161f6b35c0
honkerSK <geeksk54@gmail.com>
2020-08-30 17:49:30 +0800

    01_NativeAudioVideoDemo

---
+++ 01_NativeAudioVideoDemo/NativeAudioVideoDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NativeAudioVideoDemo
+//
+//  Created by sunke on 2020/8/30.
+//  Copyright © 2020 KentSun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SKSwiftVideoDemo
 Swift实现音视频和直播相关demo
+
+### 01_NativeAudioVideoDemo(原生音视频采集)
+
